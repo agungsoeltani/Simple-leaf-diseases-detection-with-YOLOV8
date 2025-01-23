3a18ed39dee18bf4308a783b269f7eecd7388a2e
Agung <73429948+agungsoeltani@users.noreply.github.com>
2025-01-23 23:20:57 +0700

    Update gpu_test.py

--- gpu_test.py
+++ gpu_test.py
@@ -1,3 +1,3 @@
 import torch
-print(torch.version.cuda)  # Versi CUDA yang dikenali oleh PyTorch
-print(torch.cuda.is_available())  # Memeriksa apakah GPU tersedia
+print(torch.version.cuda)
+print(torch.cuda.is_available())  
